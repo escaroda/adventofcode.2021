42548496dbba4e5062c179be48e280feada94355
Dima Escaroda <escaroda@gmail.com>
2022-01-18 10:10:47 +0300

    day21 part2 refactor & comments

--- day21/day21_part2.js
+++ day21/day21_part2.js
@@ -16,7 +16,7 @@ const moves = [ // 3^3 = 27
   [3, 1, 1], [3, 1, 2], [3, 1, 3], [3, 2, 1], [3, 2, 2], [3, 2, 3], [3, 3, 1], [3, 3, 2], [3, 3, 3],
 ];
 const reducedMoves = moves.map(combination => combination.reduce((a, b) => a + b, 0));
-const uniqueMoves = [...new Set(reducedMoves)];
+const uniqueMoves = [...new Set(reducedMoves)]; // Remove repetitions but take into account a real number of moves using ratio
 
 const getMoveRatio = (moves) => {
   const ratio = {};
@@ -26,15 +26,15 @@ const getMoveRatio = (moves) => {
   }
   return ratio
 }
-const moveRatio = getMoveRatio(reducedMoves);
+const moveRatio = getMoveRatio(reducedMoves); // Multiplier for universes since unique moves happens ratio[move] times
 
-const getCacheKey = ([s1, s2], [p1, p2]) => `${s1}-${s2}-${p1}-${p2}`; // There are ~21*21*10*10 possible game states
+const getCacheKey = ([s1, s2], [p1, p2]) => `${s1}-${s2}-${p1}-${p2}`; // There are ~20*20*10*10 possible game states
 
 
 fs.readFile(path.resolve(__dirname, './input'), 'utf8', (err, data) => {
   if (err) return console.error(err);
 
-  const positions = data.trim().split('\n').map(line => parseInt(line[line.length -1], 10));
+  const positions = data.trim().split('\n').map(line => parseInt(line[line.length -1], 10)); // [player1, player2]
 
   const cache = {};
   const play = (scores, positions) => {
@@ -43,22 +43,22 @@ fs.readFile(path.resolve(__dirname, './input'), 'utf8', (err, data) => {
     if (scores[1] >= WIN_SCORE) return [0, 1];
 
     const cacheKey = getCacheKey(scores, positions);
-    if (cache[cacheKey]) return cache[cacheKey];
+    if (cache[cacheKey]) return cache[cacheKey]; // Return already calculated result for current scores and positions if exist
 
-    let universes = [0, 0];
+    let universes = [0, 0];               // How many win universes for both players
     for (const move of uniqueMoves) {
-      const cScores = [...scores];
-      const cPositions = [...positions];
+      const scoresC = [...scores];        // Make a copy of arrays or they will mutate
+      const positionsC = [...positions];
 
-      // 1st player move
-      cPositions[0] = getPosition(cPositions[0], move);
-      cScores[0] += cPositions[0];
+      // 'Left' player move
+      positionsC[0] = getPosition(positionsC[0], move);
+      scoresC[0] += positionsC[0];
 
-      const [u1, u2] = play(cScores.reverse(), cPositions.reverse()); // Swap players to make a move for another player
-      universes[0] += u2 * moveRatio[move];                           // But swap back the results
-      universes[1] += u1 * moveRatio[move];
+      const [ uL, uR ] = play(scoresC.reverse(), positionsC.reverse()); // Swap players to make a move for the other one
+      universes[0] += uR * moveRatio[move];                             // But swap back the result
+      universes[1] += uL * moveRatio[move];
     }
-    cache[cacheKey] = universes;
+    cache[cacheKey] = universes; // Save current game sate result
     return universes
   }
   
